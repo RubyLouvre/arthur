b82e4ac22d9eac3fe3c1ee0a1e87537b685003eb
RubyLouvre <cheng5128>
2016-10-24 00:35:30 +0800

    添加依赖

---
+++ beforeIt.js
@@ -0,0 +1,15 @@
+function heredoc(fn) {
+    return fn.toString().replace(/^[^\/]+\/\*!?\s?/, '').
+            replace(/\*\/[^\/]+$/, '').trim().replace(/>\s*</g, '><')
+}
+function fireClick(el) {
+    if (el.click) {
+        el.click()
+    } else {
+//https://developer.mozilla.org/samples/domref/dispatchEvent.html
+        var evt = document.createEvent('MouseEvents')
+        evt.initMouseEvent('click', true, true, window,
+                0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        !el.dispatchEvent(evt);
+    }
+}
